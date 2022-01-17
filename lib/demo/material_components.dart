e2787b61a07cceec48b8d2dad66c3cc402db059d
jiangwenju <13075323451@163.com>
2022-01-17 21:44:07 +0800

    按钮

---
+++ lib/demo/material_components.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'button_demo.dart';
+import 'floating_action_button_demo.dart';
+import 'popup_menu_button_demo.dart';
+
+class MaterialComponents extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('MaterialComponents'),
+        elevation: 0.0,
+      ),
+      body: ListView(
+        children: [
+          ListItem(
+            title: 'Button',
+            page: ButtonDemo(),
+          ),
+          ListItem(
+            title: 'PopupMenuButton',
+            page: PopupMenuButtonDemo(),
+          ),
+          ListItem(
+            title: 'FloatingActionButton',
+            page: FloatingActionButtonDemo(),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+class _WidgetDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('WidgetDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
+class ListItem extends StatelessWidget {
+  final String title;
+  final Widget page;
+
+  ListItem({this.title, this.page});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(title),
+      onTap: () {
+        Navigator.of(context)
+            .push(MaterialPageRoute(builder: (context) => page));
+      },
+    );
+  }
+}
---
+++ lib/demo/popup_menu_button_demo.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+class PopupMenuButtonDemo extends StatefulWidget {
+  @override
+  _PopupMenuButtonDemoState createState() => _PopupMenuButtonDemoState();
+}
+
+class _PopupMenuButtonDemoState extends State<PopupMenuButtonDemo> {
+  String _currentMenuItem='Home';
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('PopupMenuButtonDemo'),
+        elevation: 0.0,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Text(_currentMenuItem),
+                PopupMenuButton(
+                  onSelected: (value) {
+                    print(value);
+                    setState(() {
+                      _currentMenuItem=value;
+                    });
+                  },
+                  itemBuilder: (BuildContext context) => [
+                    PopupMenuItem(value: 'Home', child: Text('Home')),
+                    PopupMenuItem(value: 'Discover', child: Text('Discover')),
+                    PopupMenuItem(value: 'Community', child: Text('Community')),
+                  ],
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
